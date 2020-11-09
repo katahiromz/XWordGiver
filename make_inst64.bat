b7ea9a59c78b50ff63dfb57d5cc4d6d0ad8a8bc2
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-11-09 13:58:48 +0900

    reduce installer size

--- make_inst64.bat
+++ make_inst64.bat
@@ -1,2 +1,2 @@
-candle installer64.wxs -ext WixUIExtension -ext WiXUtilExtension
-light -out xword64-4.6.0-setup.msi -cultures:ja-jp installer64.wixobj -ext WixUIExtension -ext WiXUtilExtension
+candle installer64.wxs -ext WixUIExtension
+light -out xword64-4.6.0-setup.msi -cultures:ja-jp installer64.wixobj -ext WixUIExtension
