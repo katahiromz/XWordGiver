8ffdafcaf9f592661e3bb238fca1f6a5ff6072c2
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-01-20 16:17:29 +0900

    improve dict_analyze 2

--- dict_analyze/dict_analyze.cpp
+++ dict_analyze/dict_analyze.cpp
@@ -458,16 +458,20 @@ bool DoAnalyzeDict(HWND hwnd, LPCWSTR pszFileName, const word_list_t& list, cons
             }
             tag_histgram[count] = tag;
         }
+        size_t number = 0;
         for (it_t it = tag_histgram.rbegin(); it != tag_histgram.rend(); ++it) {
             auto& pair = *it;
             size_t count = pair.first;
             auto& tag = pair.second;
             DoPrintf(hwnd, 145, pair.second.c_str(), UINT(count));
 
-            size_t needed_count = std::min(list.size() / 10, (size_t)50);
-            if (count < needed_count) {
-                auto diff = needed_count - count;
-                DoPrintf(hwnd, 144, tag.c_str(), UINT(diff), tag.c_str());
+            if (number <= 10) {
+                ++number;
+                size_t needed_count = std::min(list.size() / 10, (size_t)50);
+                if (count < needed_count) {
+                    auto diff = needed_count - count;
+                    DoPrintf(hwnd, 144, tag.c_str(), UINT(diff), tag.c_str());
+                }
             }
         }
     }
