376d6c45605ec5a595d0eae8ef9713e0f7adf9b4
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-02-14 10:18:16 +0900

    version up to 4.7.0

--- make_inst32.bat
+++ make_inst32.bat
@@ -1,2 +1,2 @@
 candle installer32.wxs -ext WixUIExtension
-light -out xword32-4.6.9-setup.msi -cultures:ja-jp installer32.wixobj -ext WixUIExtension
+light -out xword32-4.7.0-setup.msi -cultures:ja-jp installer32.wixobj -ext WixUIExtension
--- make_inst64.bat
+++ make_inst64.bat
@@ -1,2 +1,2 @@
 candle installer64.wxs -ext WixUIExtension
-light -out xword64-4.6.9-setup.msi -cultures:ja-jp installer64.wixobj -ext WixUIExtension
+light -out xword64-4.7.0-setup.msi -cultures:ja-jp installer64.wixobj -ext WixUIExtension
