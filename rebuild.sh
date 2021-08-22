eea9e752d0d1763d2bb517fbe0b7d4762655be1a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-08-22 20:02:50 +0900

    improve rebuild.sh 2

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm CMakeCache.txt
-CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -G "MSYS Makefiles" .
+CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -G "MSYS Makefiles" .
 make -j4
