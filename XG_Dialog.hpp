45eb441960a25d6b5b09c456381230b78f93dad3
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-11-13 19:19:13 +0900

    UTF-8 BOM

--- XG_Dialog.hpp
+++ XG_Dialog.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #ifndef _INC_WINDOWS
     #include <windows.h>
--- XG_WordListDialog.hpp
+++ XG_WordListDialog.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "XG_Dialog.hpp"
 #include "crossword_generation.hpp"
