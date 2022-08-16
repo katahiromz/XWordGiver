d8612c895b3afbf5f8acde26e8664621f6a4aa22
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-08-16 19:20:28 +0900

    tooltips for added buttons

--- resource.h
+++ resource.h
@@ -230,6 +230,8 @@
 #define IDS_TT_GENERATEREPEATEDLY           10154
 #define IDS_TT_SOLVEREPEATEDLY              10167
 #define IDS_TT_SOLVEREPEATEDLYNOADDBLACK    10168
+#define IDS_TT_UNDO                         10106
+#define IDS_TT_REDO                         10107
 
 #define ID_NEW                              100
 #define ID_GENERATE                         101
