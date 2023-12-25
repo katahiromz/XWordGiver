dc8c5b6c5d0935625713977b74311c03997bbbc1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-12-25 14:48:50 +0900

    add HowToBuild.txt 6

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm CMakeCache.txt
-CC=gcc CXX=g++ cmake -DCMAKE_BUILD_TYPE=Release -G "MSYS Makefiles" .
-make -j4
+CC=gcc CXX=g++ cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" .
+mingw32-make.exe -j4
 strip *.exe
