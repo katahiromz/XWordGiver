5bc612e29213b6ebbe8e987c7fc4f27ae7c38123
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-04-01 13:52:49 +0900

    move default save location 2

--- GUI.cpp
+++ GUI.cpp
@@ -1166,7 +1166,7 @@ bool __fastcall XgLoadSettings(void)
         ::SHGetSpecialFolderLocation(nullptr, CSIDL_PERSONAL, &pidl);
         ::SHGetPathFromIDListW(pidl, szPath);
         ::CoTaskMemFree(pidl);
-        PathAppendW(szPath, L"XWordGiver files");
+        PathAppendW(szPath, L"XWordGiver");
         xg_dirs_save_to.emplace_back(szPath);
     }
 
