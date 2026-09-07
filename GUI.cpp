031e21230b77da6bc01b8040f5e2feebea402bf8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-09-07 12:00:37 +0900

    implementing AI Helper 11

--- GUI.cpp
+++ GUI.cpp
@@ -6294,6 +6294,9 @@ std::wstring XgMakeInitialQuestion_ja(void)
         L"システムはあなたのコマンド出力「【Cq: YYY】」でCqを「YYY」に書き換えます(qは任意の自然数、YYYは新しい列文字列)。"
         L"あなたは盤面のサイズを変更することはできません。"
 
+        L"アプリの使い方を聞かれたら、「付属のREADMEを読んでね」と答えてください。"
+        L"黒マスルールについて聞かれたら、「付属のPolicy-JPN.txtを読んでね」と答えてください。"
+
         L"まずは60字程度のあいさつをして、あなたができることを簡単に説明してください。"
     L"*) ";
     return str;
@@ -6324,6 +6327,9 @@ std::wstring XgMakeInitialQuestion_en(void)
         L"The system will rewrite Rp to \"XXX\" when your command output is \"【Rp: XXX】\" (p is any natural number, XXX is the new row string)."
         L"The system will rewrite Cq to \"YYY\" when your command output is \"【Cq: YYY】\" (q is any natural number, YYY is the new column string)."
         L"You cannot change the size of the board."
+
+        L"If asked about how to use the app, please answer \"Please read the included README.\" "
+        L"If asked about the black-cell rules, please answer \"Please read the included Policy-ENG.txt.\" "
 
         L"First, please provide a greeting of around 60 characters and briefly explain what you can do."
     L"*) ";
