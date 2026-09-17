a9525cd62441817142ce5502eef48ef011deeb61
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-09-17 14:10:41 +0900

    季節の判定を改良

--- AIHelper.cpp
+++ AIHelper.cpp
@@ -501,11 +501,11 @@ void XgJudgeSeasonOfBoard(void)
 
 	PCWSTR line;
 	if (XgIsUserJapanese()) {
-		line = L"盤面をご覧になり、盤面の季節を「冬～春」「春」「春～夏」「夏」「夏～秋」"
+		line = L"盤面の季節を「冬～春」「春」「春～夏」「夏」「夏～秋」"
 		       L"「秋」「秋～冬」「冬」「無季」「分類不可」のいずれかに分類してください。"
-		       L"また、季節らしさを1～5で評価してください。";
+		       L"また、季節感を1～5で評価してください。";
 	} else {
-		line = L"Please look at the board and classify the season depicted into one of the following categories: 'Winter–Spring,' 'Spring,' "
+		line = L"Please classify the season of the board depicted into one of the following categories: 'Winter–Spring,' 'Spring,' "
 		       L"'Spring–Summer,' 'Summer,' 'Summer–Autumn,' 'Autumn,' 'Autumn–Winter,' 'Winter,' 'Seasonless,' or 'Unclassifiable.' "
 		       L"Please also rate the sense of the season on a scale of 1 to 5. ";
 	}
