033ada34ae773307f1b1b6a8a08203ccf85c4f25
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-09-18 21:30:56 +0900

    adjustment of AI models 2

--- AIHelper.cpp
+++ AIHelper.cpp
@@ -1797,6 +1797,7 @@ struct AIModelList
 };
 
 // TODO: １ヵ月ごとに以下の既知のAIモデル名一覧を更新せよ。可能ならば先頭を無料モデルにしておくこと。
+// 2026年9月18日最新(?)
 static const PCWSTR s_chatgptModels[] = {
     L"gpt-4o-mini", L"gpt-4.1", L"gpt-5.6-luna", L"gpt-5.6-sol", L"gpt-5.6-terra", L"gpt-6-astra",
 };
